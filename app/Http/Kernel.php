dd7b18b493e68b3f68c17837d409577371ea20c9
kapitanluffy <kapitanluffy@gmail.com>
2019-03-15 03:38:10 +0000

    Remove unused middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'env.exists' => \App\Http\Middleware\EnvExists::class,
-        'env.load_root' => \App\Http\Middleware\EnvLoadRoot::class,
     ];
 
     /**
