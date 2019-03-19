d9950bde8edcc73f85a00bf35524f7ceec0818d6
kapitanluffy <kapitanluffy@gmail.com>
2019-03-19 14:57:01 +0000

    Update EnvFile test

--- tests/Unit/EnvFileTest.php
+++ tests/Unit/EnvFileTest.php
@@ -9,6 +9,19 @@ use App\EnvFile\EnvFile;
 
 class EnvFileTest extends TestCase
 {
+    public function contentProvider()
+    {
+        return [
+            [
+                "# this is a comment",
+                "line1" => "the quick",
+                "line2" => "brown fox",
+                "line3" => "jumps over",
+                "line4" => "the lazy dog"
+            ]
+        ];
+    }
+
     public function testPath()
     {
         $env = new EnvFile("foo");
@@ -16,71 +29,92 @@ class EnvFileTest extends TestCase
         $this->assertEquals($env->path(), "foo");
     }
 
-    public function testContents()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testContents($content)
     {
-        $env = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $env = new EnvFile("foo", $content);
 
         $this->assertEquals($env->contents(), "LINE1=the quick\nLINE2=brown fox\nLINE3=jumps over\nLINE4=the lazy dog");
     }
 
-    public function testWithContents()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testWithContents($content)
     {
         $env = new EnvFile("foo");
-        $dupe = $env->withContents(["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $dupe = $env->withContents($content);
 
         $this->assertNotEquals($env, $dupe);
     }
 
-    public function testHas()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testHas($content)
     {
-        $env = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $env = new EnvFile("foo", $content);
 
-        $this->assertFalse($env->has(0));
+        $this->assertFalse($env->has("0"));
         $this->assertTrue($env->has("line1"));
         $this->assertTrue($env->has("\tline1\n"));
         $this->assertTrue($env->has("\tLiNe1\n"));
     }
 
-    public function testSet()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testSet($content)
     {
-        $env = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $env = new EnvFile("foo", $content);
 
-        $env->set(0, "# change the comment");
+        $env->set("0", "# change the comment");
         $env->set("line1", "foo");
         $env->set("\tline2\n", "bar");
         $env->set("\tLiNe3\n", "baz");
 
-        $this->assertNull($env->get(0));
+        $this->assertNull($env->get("0"));
         $this->assertEquals($env->get("line1"), "foo");
         $this->assertEquals($env->get("line2"), "bar");
         $this->assertEquals($env->get("line3"), "baz");
     }
 
-    public function testGet()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testGet($content)
     {
-        $env = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $env = new EnvFile("foo", $content);
 
-        $this->assertNull($env->get(0));
+        $this->assertNull($env->get("0"));
         $this->assertEquals($env->get("line1"), "the quick");
         $this->assertEquals($env->get("\tline1\n"), "the quick");
         $this->assertEquals($env->get("\tLiNe1\n"), "the quick");
     }
 
-    public function testDelete()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testDelete($content)
     {
-        $env = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
-        $dupe = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $env = new EnvFile("foo", $content);
+        $dupe = new EnvFile("foo", $content);
 
-        $env->delete(0);
+        $env->delete("0");
         $this->assertEquals($env, $dupe);
 
         $env->delete("line1");
         $this->assertNotEquals($env, $dupe);
     }
 
-    public function testToArray()
+    /**
+     * @dataProvider contentProvider
+     */
+    public function testToArray($content)
     {
-        $env = new EnvFile("foo", ["# this is a comment", "line1" => "the quick", "line2" => "brown fox", "line3" => "jumps over", "line4" => "the lazy dog"]);
+        $env = new EnvFile("foo", $content);
 
         $array = $env->toArray();
         $dupe = new EnvFile("bar", $array);
